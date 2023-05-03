428ab94dc4b96b73cf8d99fe586d24d215cf79a9
JinPyoK <kjp00552277@gmail.com>
2023-05-03 22:46:57 +0900

    Update home_screen.dart

--- whiteus/lib/HomeScreen/home_screen.dart
+++ whiteus/lib/HomeScreen/home_screen.dart
@@ -18,7 +18,7 @@ class HomeScreen extends StatelessWidget {
       body: Column(
         children: [
           const SizedBox(
-            height: 70,
+            height: 100,
           ),
           Expanded(child: makeList(imageTitle, imagePath)),
           //Expanded(child: makeList(imageclass))
