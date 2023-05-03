618308739da4dacdbe2802d4dbe8630a7ac0172a
JinPyoK <kjp00552277@gmail.com>
2023-05-03 22:38:26 +0900

    0503

---
+++ whiteus/lib/CommunityScreen/com_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ComScreen extends StatelessWidget {
+  const ComScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: const Text("Community Screen"),
+      ),
+    );
+  }
+}
