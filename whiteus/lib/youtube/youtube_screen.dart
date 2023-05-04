93361e88085b7a1e8184414fac04894169318442
ksj7385 <117991880+ksj7385@users.noreply.github.com>
2023-05-05 05:01:57 +0900

    클래스 } 오류 해결
    -클래스 } 위치 조절 및 코드 정리

--- whiteus/lib/youtube/youtube_screen.dart
+++ whiteus/lib/youtube/youtube_screen.dart
@@ -6,93 +6,82 @@ import 'dart:developer';
 import 'DataModel.dart';
 import 'RestClient.dart';
 
-void main() => runApp(new WidgetDemo());
+void main() => runApp(const WidgetDemo());
 
-class WidgetDemo extends StatefulWidget{
+class WidgetDemo extends StatefulWidget {
+  const WidgetDemo({super.key});
+
   @override
   State<StatefulWidget> createState() => WidgetDemoState();
 }
 
-class WidgetDemoState extends State<WidgetDemo>{
+class WidgetDemoState extends State<WidgetDemo> {
   late VideoItems response;
   bool isLoading = false;
 
   @override
-  Widget build(BuildContext context){
-    return new MaterialApp(
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: 'Retrofit Demo',
-      home: new Scaffold(
-        appBar: new AppBar(
+      home: Scaffold(
+        appBar: AppBar(
           title: const Text('Retrofit Demo'),
         ),
         body: getPage(),
       ),
     );
   }
-}
 
-getData(){
-  final dio=Dio();
-  final client=RestClient(dio);
-  client.getYouTubeAPI("dyRsYk0LyA8",
-      "AIzaSyAvmIOOgWdDr6dieGgUK40wuUjmV8i_nAA",
-      "items(id,snippet(publishedAt,title,thumbnails),statistics(viewCount))",
-      "snippet,statistics").then((it){
-        setState((){
-          response=((it['items'] as List)[0]) as VideoItems;
-          isLoading=true;
-          log('${response.snippet.title}');
-        });
-  });
-}
-
-Widget getPage(){
-  late Widget page;
-  if(!isLoading){
-    page = page0();
-    getData();
-  }else{
-    page = page1();
+  getData() {
+    final dio = Dio();
+    final client = RestClient(dio);
+    client
+        .getYouTubeAPI(
+            "dyRsYk0LyA8",
+            "AIzaSyAvmIOOgWdDr6dieGgUK40wuUjmV8i_nAA",
+            "items(id,snippet(publishedAt,title,thumbnails),statistics(viewCount))",
+            "snippet,statistics")
+        .then((it) {
+      setState(() {
+        response = ((it['items'] as List)[0]) as VideoItems;
+        isLoading = true;
+        log(response.snippet.title);
+      });
+    });
   }
-  return page;
-}
 
-Container page0(){
-  return Container(
-    alignment: Alignment.center,
-    child: Text(
-      'Loading...',style: TextStyle(color: Colors.blue, fontSize: 30),
-    ),
-  );
-}
+  Widget getPage() {
+    late Widget page;
+    if (!isLoading) {
+      page = page0();
+      getData();
+    } else {
+      page = page1();
+    }
+    return page;
+  }
 
-ListView page1(){
-  return ListView(
-    children: <Widget>[
-      ListTile(
-        leading: Image.network(response.snippet.thumbnails.medium.url),
-        title: Text(response.snippet.title),
-        subtitle: Text('View : ${response.statistics.viewCount}'),
-        trailing: Icon(Icons.favorite_border),
-        onTap: () {},
+  Container page0() {
+    return Container(
+      alignment: Alignment.center,
+      child: const Text(
+        'Loading...',
+        style: TextStyle(color: Colors.blue, fontSize: 30),
       ),
-    ],
-  );
+    );
+  }
+
+  ListView page1() {
+    return ListView(
+      children: <Widget>[
+        ListTile(
+          leading: Image.network(response.snippet.thumbnails.medium.url),
+          title: Text(response.snippet.title),
+          subtitle: Text('View : ${response.statistics.viewCount}'),
+          trailing: const Icon(Icons.favorite_border),
+          onTap: () {},
+        ),
+      ],
+    );
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
