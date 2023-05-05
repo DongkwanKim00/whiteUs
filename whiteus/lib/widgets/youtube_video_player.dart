328426870f56a760a11d0da2fa4f445c21bc894b
ksj7385 <117991880+ksj7385@users.noreply.github.com>
2023-05-05 17:23:02 +0900

    유튜브 영상 테스트
    -유튜브 위젯 클래스 추가
    -홈스크린에 예시 화면 추가

---
+++ whiteus/lib/widgets/youtube_video_player.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/widgets.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+class YouTubeVideoPlayer extends StatefulWidget {
+  final String videoId;
+
+  const YouTubeVideoPlayer({super.key, required this.videoId});
+
+  @override
+  _YouTubeVideoPlayerState createState() => _YouTubeVideoPlayerState();
+}
+
+class _YouTubeVideoPlayerState extends State<YouTubeVideoPlayer> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController(
+      initialVideoId: widget.videoId,
+      flags: const YoutubePlayerFlags(
+        autoPlay: true,
+        mute: false,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return YoutubePlayerBuilder(
+      player: YoutubePlayer(
+        controller: _controller,
+        showVideoProgressIndicator: true,
+      ),
+      builder: (context, player) {
+        return Column(
+          children: [
+            // some widgets
+            player,
+            // some other widgets
+          ],
+        );
+      },
+    );
+  }
+}
