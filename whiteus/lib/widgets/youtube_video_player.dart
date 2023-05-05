ce517c136b1dc52458903a9642a073f028dac9b6
ksj7385 <117991880+ksj7385@users.noreply.github.com>
2023-05-05 21:09:38 +0900

    유튜브 api 사용
    조회수랑 이름 추가

--- whiteus/lib/widgets/youtube_video_player.dart
+++ whiteus/lib/widgets/youtube_video_player.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import '../services/youtube_service.dart'; // 앱 이름을 사용하세요
 
 class YouTubeVideoPlayer extends StatefulWidget {
   final String videoId;
@@ -12,6 +13,7 @@ class YouTubeVideoPlayer extends StatefulWidget {
 
 class _YouTubeVideoPlayerState extends State<YouTubeVideoPlayer> {
   late YoutubePlayerController _controller;
+  late Future<Map<String, dynamic>> videoInfo;
 
   @override
   void initState() {
@@ -23,6 +25,7 @@ class _YouTubeVideoPlayerState extends State<YouTubeVideoPlayer> {
         mute: false,
       ),
     );
+    videoInfo = fetchVideoInfo(widget.videoId,'AIzaSyAvmIOOgWdDr6dieGgUK40wuUjmV8i_nAA');
   }
 
   @override
@@ -33,65 +36,48 @@ class _YouTubeVideoPlayerState extends State<YouTubeVideoPlayer> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 10,
-              offset: const Offset(5, 5),
-              color: Colors.black.withOpacity(0.3),
+    return Row(
+      children: [
+        Expanded(
+          child: YoutubePlayerBuilder(
+            player: YoutubePlayer(
+              controller: _controller,
+              showVideoProgressIndicator: true,
             ),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(15),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                flex: 3,
-                child: AspectRatio(
-                  aspectRatio: 16 / 9,
-                  child: YoutubePlayerBuilder(
-                    player: YoutubePlayer(
-                      controller: _controller,
-                      showVideoProgressIndicator: true,
-                      bottomActions: [
-                        CurrentPosition(),
-                        ProgressBar(isExpanded: true),
-                        RemainingDuration(),
-                        FullScreenButton(),
-                      ],
-                    ),
-                    builder: (context, player) {
-                      return player;
-                    },
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: Container(
-                  padding: const EdgeInsets.all(16),
-                  color: Colors.white,
-                  child: const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Information', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                      SizedBox(height: 16),
-                      Text('여기에 각종 정보를 출력합니다.'),
-                      // 다른 정보를 출력하려면 여기에 추가하세요
-                    ],
-                  ),
-                ),
-              ),
-            ],
+            builder: (context, player) {
+              return player;
+            },
           ),
         ),
-      ),
+        Expanded(
+          child: FutureBuilder<Map<String, dynamic>>(
+            future: videoInfo,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasData) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        snapshot.data!['title'],
+                        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '조회수: ${snapshot.data!['viewCount']}',
+                        style: const TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  );
+                } else {
+                  return const Text('영상 정보를 불러오지 못했습니다.');
+                }
+              } else {
+                return const CircularProgressIndicator();
+              }
+            },
+          ),
+        ),
+      ],
     );
   }
 }
