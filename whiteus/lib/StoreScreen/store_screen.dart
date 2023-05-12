26f280717ad0aac01ea008a592803e9b1c1afe7c
DongkwanKim00 <dkkim_it@naver.com>
2023-05-12 16:47:21 +0900

    파일업로드 성공
    근데 파일 이름을 내가 임의로 지정해둔대로만 올라감. 해결중...

--- whiteus/lib/StoreScreen/store_screen.dart
+++ whiteus/lib/StoreScreen/store_screen.dart
@@ -14,6 +14,7 @@
 // }
 
 import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
@@ -29,78 +30,73 @@ class _Mp3UploaderDownloaderState extends State<Mp3UploaderDownloader> {
 
   // Method to upload an mp3 file to the server
   Future<void> uploadMp3(File file) async {
-    final url = 'http://localhost:8000/upload'; // Replace with your own upload API url
-    final request = http.MultipartRequest('POST', Uri.parse(url));
-    request.files.add(await http.MultipartFile.fromPath('mp3', file.path));
-    final response = await request.send();
-    if (response.statusCode == 200) {
-      print('Mp3 file uploaded successfully');
-    } else {
-      print('Error uploading mp3 file');
-    }
+
+    FirebaseStorage.instance
+        .ref('/whiteus/')
+        .putFile(file);
+
+    // final storage = FirebaseStorage.instance;
   }
 
-  // Method to download an mp3 file from the server
-  Future<void> downloadMp3() async {
-    final url = 'http://localhost:8000/download'; // Replace with your own download API url
-    final response = await http.get(Uri.parse(url));
-    final bytes = response.bodyBytes;
-    final dir = await getApplicationDocumentsDirectory();
-    final file = File('${dir.path}/downloaded.mp3');
-    await file.writeAsBytes(bytes);
-    setState(() {
-      _filePath = file.path;
-    });
-    print('Mp3 file downloaded successfully');
-  }
-
-  // Method to pick an mp3 file from the device's file system
-  Future<void> pickMp3() async {
-    final result = await FilePicker.platform.pickFiles(
-      type: FileType.custom,
-      allowedExtensions: ['mp3'],
-    );
-    if (result != null) {
-      final file = File(result.files.single.path!);
+    // Method to download an mp3 file from the server
+    Future<void> downloadMp3() async {
+      final url = 'http://localhost:8000/download'; // Replace with your own download API url
+      final response = await http.get(Uri.parse(url));
+      final bytes = response.bodyBytes;
+      final dir = await getApplicationDocumentsDirectory();
+      final file = File('${dir.path}/downloaded.mp3');
+      await file.writeAsBytes(bytes);
       setState(() {
         _filePath = file.path;
       });
+      print('Mp3 file downloaded successfully');
+    }
+
+    // Method to pick an mp3 file from the device's file system
+    Future<void> pickMp3() async {
+
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+          type: FileType.custom,
+          allowedExtensions: ['mp3']
+      );
+      if(result != null) {
+        File file = File(result.files.single.path.toString());
       await uploadMp3(file);
+      }
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Mp3 Uploader/Downloader'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  await pickMp3();
+                },
+                child: Text('Upload Mp3'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  await downloadMp3();
+                },
+                child: Text('Download Mp3'),
+              ),
+              if (_filePath.isNotEmpty) ...[
+                SizedBox(height: 16),
+                Text(
+                  'Selected Mp3 File Path: $_filePath',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ],
+          ),
+        ),
+      );
     }
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Mp3 Uploader/Downloader'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                await pickMp3();
-              },
-              child: Text('Upload Mp3'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                await downloadMp3();
-              },
-              child: Text('Download Mp3'),
-            ),
-            if (_filePath.isNotEmpty) ...[
-              SizedBox(height: 16),
-              Text(
-                'Selected Mp3 File Path: $_filePath',
-                style: TextStyle(fontSize: 16),
-              ),
-            ],
-          ],
-        ),
-      ),
-    );
-  }
-}
